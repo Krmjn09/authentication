cf9567b764d589077cfecf9a4694972550483613
Krmjn09 <kmahajan1_be22@thapar.edu>
2024-04-24 19:20:55 +0530

    commited

--- client/src/Journal.jsx
+++ client/src/Journal.jsx
@@ -1,8 +1,9 @@
 // import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import Card from "react-bootstrap/Card";
+// import Card from "react-bootstrap/Card";
 
+import "./Journal.css";
 const JournalPage = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -113,6 +114,52 @@ const JournalPage = () => {
           )}
         </div>
       </nav>
+      <div className="wrapper">
+        <div className="card">
+          <img src="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&amp;fit=crop&amp;w=667&amp;q=80&amp;ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" />
+          <div className="info">
+            <h1>Mountain</h1>
+            <p>
+              Lorem Ipsum is simply dummy text from the printing and typeseting
+              industry
+            </p>
+            <button>Read More</button>
+          </div>
+        </div>
+        <div className="card">
+          <img src="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&amp;fit=crop&amp;w=667&amp;q=80&amp;ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" />
+          <div className="info">
+            <h1>Mountain</h1>
+            <p>
+              Lorem Ipsum is simply dummy text from the printing and typeseting
+              industry
+            </p>
+            <button>Read More</button>
+          </div>
+        </div>
+        <div className="card">
+          <img src="https://images.unsplash.com/photo-1425342605259-25d80e320565?auto=format&amp;fit=crop&amp;w=750&amp;q=80&amp;ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" />
+          <div className="info">
+            <h1>Road</h1>
+            <p>
+              Lorem Ipsum is simply dummy text from the printing and typeseting
+              industry
+            </p>
+            <button>Read More</button>
+          </div>
+        </div>
+        <div className="card">
+          <img src="https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?auto=format&amp;fit=crop&amp;w=311&amp;q=80&amp;ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" />
+          <div className="info">
+            <h1>Protester</h1>
+            <p>
+              Lorem Ipsum is simply dummy text from the printing and typeseting
+              industry
+            </p>
+            <button>Read More</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
